89e623b48614e4e29877aad0d62ccb7c4f53b987
Rabia Arslan <rabiaarslann.11@gmail.com>
2021-02-06 22:08:22 +0300

    first commit

---
+++ List_MyDictonary/List_Dictionary_Day4/Dictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List_Dictionary_Day4
+{
+    class Dictionary<Tkey,Tvalue>
+    {
+            Tkey []
+            keys;
+            Tvalue []
+            values;
+
+        public Dictionary()
+        {
+            keys = new Tkey[0];
+            values = new Tvalue[0];
+
+        }
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            Tkey[] tempTkey = keys;
+            keys = new Tkey[keys.Length + 1];
+            Tvalue[] tempTvalue = values;
+            values = new Tvalue[values.Length + 1];
+            for (int i = 0; i < tempTkey.Length; i++)
+            {
+                keys[i] = tempTkey[i];
+                values[i] = tempTvalue[i];
+
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public Tkey[] key { get { return keys; } }
+        public Tvalue[] value { get { return values; } }
+        
+        public int Length { get { return keys.Length; } }
+
+
+
+    }
+}
---
+++ List_MyDictonary/List_Dictionary_Day4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace List_Dictionary_Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> days = new Dictionary<int, string>();
+            days.Add(1, "Pazartesi");
+            days.Add(2, "Salı");
+            days.Add(3, "Çarşamba");
+            days.Add(4, "Perşembe");
+            days.Add(5, "Cuma");
+            days.Add(6, "Cumartesi");
+            days.Add(7, "Pazar");
+           
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine(days.key[i] + ".gün " + days.value[i]);
+           
+            }
+        }
+    }
+}
